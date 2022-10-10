0635b8747851220c9aa4f080717f97ee8b0b3044
s473581 <michalikoliwiamagdalena@gmail.com>
2022-10-10 15:10:09 +0200

    moje zmiany pierwsze zajęcia

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,12 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+    static Logger log = Logger.getLogger("name");
 
     public static void main(String [ ] args) {
+        log.info("Witaj !!");
         System.out.println("Hello! Witaj!");
     }
 }
